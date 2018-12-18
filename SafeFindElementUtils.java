2c5f0d871d6bff209a18aa61d51c5a5915739850
Arthur Reynolds <33764105+awreynold@users.noreply.github.com>
2018-12-18 12:45:50 -0600

    Update SafeFindElementUtils.java

--- SafeFindElementUtils.java
+++ SafeFindElementUtils.java
@@ -3,6 +3,12 @@
   instead of throwing an exception.
 */
 
+/**
+  safely tries to find and return the webelement that matches the xpath value passed in using the passed in driver variable.
+  @param driver -> WebDriver
+  @param xpath -> String
+  @return -> WebElement
+*/
 public static WebElement safeFindElementByXpath(WebDriver driver, String xpath){
   if(xpath.isEmpty() || driver == null)
       return null;
@@ -10,5 +16,21 @@ public static WebElement safeFindElementByXpath(WebDriver driver, String xpath){
      return driver.findElement(By.xpath(xpath));
   }catch(Exception ex){
      return null;
+  }
+}
+
+/**
+  safely tries to find and return the webelement that matches the xpath value passed in using the passed in parent webelement.
+  @param parent -> WebElement
+  @param xpath -> String
+  @return -> WebElement
+*/
+public static WebElement safeFindElementByXpathWithParent(WebElement parent, Stirng xpath){
+ if(xpath.isEmpty() || driver == null)
+   return null;
+  try{
+    return parent.findElement(By.xpath(xpath));
+  }catch(Exception ex){
+    return null;
   }
 }
