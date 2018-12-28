65c80b3a56abe3501ff2b25a4d9d87ea4cf7cb8f
Arthur Reynolds <33764105+awreynold@users.noreply.github.com>
2018-12-28 10:15:12 -0600

    Update SafeElementUtilities.java

--- SafeElementUtilities.java
+++ SafeElementUtilities.java
@@ -1,3 +1,32 @@
 public static boolean safeElementIsDisplayed(WebElement element){
   if(!element == null){
-    
+    try{
+      return element.isDisplayed();
+    }catch(Exception ex){
+      return false;
+    }
+  }
+  return false;
+}
+
+public static boolean safeElementIsSelected(WebElement element){
+  if(!element == null){
+    try{
+      return element.isSelected();
+    }catch(Exception ex){
+      return false;
+    }
+  }
+  return false;
+}
+
+public static boolean safeElementIsEnabled(WebElement element){
+  if(!element == null){
+    try{
+      return element.isEnabled();
+    }catch(Exception ex){
+      return false;
+    }
+  }
+  return false;
+}
