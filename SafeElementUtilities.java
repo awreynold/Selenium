49060bad61a4f71a0540c50ff43275e9938bc35f
Arthur Reynolds <33764105+awreynold@users.noreply.github.com>
2018-12-28 10:50:46 -0600

    Update SafeElementUtilities.java

--- SafeElementUtilities.java
+++ SafeElementUtilities.java
@@ -1,3 +1,8 @@
+/**
+*checks to see if the passed in WebElement is displayed or not. if the element itself is null false is returned.
+*@param element -> WebElement
+*@return -> boolean
+*/
 public static boolean safeElementIsDisplayed(WebElement element){
   if(!element == null){
     try{
@@ -9,6 +14,11 @@ public static boolean safeElementIsDisplayed(WebElement element){
   return false;
 }
 
+/**
+*checks to see if the passed in WebElement is selected or not. if the element itself is null false is returned.
+*@param element -> WebElement
+*@return -> boolean
+*/
 public static boolean safeElementIsSelected(WebElement element){
   if(!element == null){
     try{
@@ -20,6 +30,11 @@ public static boolean safeElementIsSelected(WebElement element){
   return false;
 }
 
+/**
+*checks to see if the passed in WebElement is enabled or not. if the element itself is null false is returned.
+*@param element -> WebElement
+*@return -> boolean
+*/
 public static boolean safeElementIsEnabled(WebElement element){
   if(!element == null){
     try{
@@ -30,3 +45,39 @@ public static boolean safeElementIsEnabled(WebElement element){
   }
   return false;
 }
+
+/**
+*safely attempts to simulate a mouse courser moving to the passed in Webelement. if the element or the driver are null then false is
+*returned
+*@param driver -> WebDriver
+*@param element -> WebElement
+*/
+public static boolean safeMoveTo(WebDriver driver, WebElement element){
+  if(!element == null || driver == null){
+    try{
+      Actions action = new Actions(driver);
+      action.moveToElement(element).build().perform();
+      return true;
+    }catch(Exception ex){
+      return false;
+    }
+  }
+  return false;
+}
+
+/**
+*safely attempts to simulate a mouse click on the passed in WebElement. if the element is null then false is returned
+*@param element -> WebElement
+*/
+public static boolean safeClick(WebElement element){
+  if(!element == null){
+    try{
+      element.click();
+      return true;
+    }catch(Exception ex){
+      return false;
+    }
+  }
+  return false;
+}
+  
